cbe95689860627f147688e2ec120c4edd3e2042a
David Yunevich <f3oall@gmail.com>
2018-04-19 15:08:10 +0300

    add support of multi brackets

--- src/classes/Expression.js
+++ src/classes/Expression.js
@@ -2,77 +2,58 @@ import Fraction from './Fraction'
 
 export default class Expression {
   constructor(value) {
-    if (typeof value === 'string') {
-      this.value = value
-      this.isFilled = true
-    } else {
-      this.value = ''
-      this.isFilled = value.every(el => {
-        if (!el.isFilled) return false
-        this.value += el.toString()
-        return true
-      })
-    }
+    this.value = value
   }
   get separator() {
     if (/[+]/.test(this.value)) return '+'
-    if (/[-]/.test(this.value)) return '-'
-    if (/[/]/.test(this.value)) return '/'
+    if (/[=]/.test(this.value)) return '='
     if (/[*]/.test(this.value)) return '*'
+    if (/[/]/.test(this.value)) return '/'
     return false
   }
   get hasBrackets() {
     return /[(|)]/.test(this.value)
   }
-  isFraction() {
+  get isFraction() {
     return /^[^:]*:[^:]*$/.test(this.value)
   }
-  isSign(str) {
-    return ['+', '-', '*', '/'].includes(str)
-  }
   get result() {
-    if (!this.isFilled) return false
     if (this.hasBrackets) return this.computeBrackets()
     return this.compute()
   }
+
   compute() {
     let arr = []
     arr = this.separator ? this.value.split(this.separator) : [this.value]
-    return this.getFraction(arr, 0, this.separator)
+    return Expression.getFraction(arr, arr.length - 1, this.separator)
   }
   computeBrackets() {
-    let arr = this.parseBrackets()
-    let fractions = arr.map(el => {
-      let fr = new Expression(el.el).result
-      if (el.sign) fr.sign = el.sign
-      return fr
+    let reg = /\(([^()]+)\)/g
+    let match
+    let matched = []
+    while ((match = reg.exec(this.value))) {
+      matched.push({ selector: match[0], exp: match[1] })
+    }
+    matched.forEach(el => {
+      let result = new Expression(el.exp).result.toString()
+      this.value = this.value.replace(el.selector, result.slice(0, -1))
     })
-    return new Expression(fractions).result
+    return new Expression(this.value).result
   }
-  parseBrackets() {
-    let arr = []
-    this.value.split(/[(|)]/).forEach(el => {
-      if (!el.length) return
-      if (this.isSign(el[0])) {
-        arr[arr.length - 1].sign = el[0]
-        el = el.substring(1)
-      }
-      arr.push({ el })
-    })
-    return arr
-  }
-  getFraction(arr, idx, sign) {
-    let exp = new Expression(arr[idx])
 
+  static getFraction(arr, idx, sign) {
     let fr
-    if (exp.isFraction()) {
+    let exp = new Expression(arr[idx])
+    if (exp.isFraction) {
       fr = new Fraction(exp.value, sign)
     } else {
       fr = exp.result
-      fr.sign = sign
+      if (sign) fr.sign = sign
     }
-
-    if (arr.length - 1 === idx) return fr
-    return fr.compute(this.getFraction(arr, idx + 1, sign).value)
+    if (idx === 0) return fr
+    return this.getFraction(arr, idx - 1, sign).compute(fr)
+  }
+  static isSign(str) {
+    return ['+', '=', '*', '/'].includes(str)
   }
 }
